7c7e3a0c3ecd349fd215d5ebc8aad7de69213f43
_ <55923312+mr0a@users.noreply.github.com>
2021-01-30 09:40:56 +0530

    Login with Roll number and pass

---
+++ ReactLoginv2/precache-manifest.04e0d6997653431732e25c604353d378.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3173531c124d96ec4fef5c1a8611a12e",
+    "url": "/ReactLoginv2/index.html"
+  },
+  {
+    "revision": "8803437d069ce03dc486",
+    "url": "/ReactLoginv2/static/css/main.00b4c0e9.chunk.css"
+  },
+  {
+    "revision": "d5875db0ea4acc94c945",
+    "url": "/ReactLoginv2/static/js/2.1af61331.chunk.js"
+  },
+  {
+    "revision": "8803437d069ce03dc486",
+    "url": "/ReactLoginv2/static/js/main.cb3e8767.chunk.js"
+  },
+  {
+    "revision": "857dd8b2731b0c7be51d",
+    "url": "/ReactLoginv2/static/js/runtime~main.275adc1d.js"
+  }
+]);
